de38c0fc278f77305e9e4a2f9654a700c71f03bd
anton-tt <carduelis@yandex.ru>
2024-09-07 21:17:49 +0700

    fix: исправить недочёты

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,14 +1,13 @@
-require('dotenv').config();
-dotenv.config({ path: "./.env.deploy" });
+require('dotenv').config({ path: './.env.deploy' });
 
 const {
   DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPOSITORY, DEPLOY_REF,
 } = process.env;
 
 module.exports = {
-  apps : [{
-    name   : "mesto",
-    script : "dist/app.js"
+  apps: [{
+    name: 'mesto-backend',
+    script: 'dist/app.js',
   }],
   deploy: {
     production: {
@@ -21,4 +20,4 @@ module.exports = {
       'post-deploy': 'cd backend && pwd && npm ci && npm run build  && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
-}
+};
