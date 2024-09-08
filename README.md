144177cfb046f27d86b7a6e9c778d74aed1ce11c
anton-tt <carduelis@yandex.ru>
2024-09-08 14:16:27 +0700

    refactor: Добавить энтрипоинт для краштеста, публичный IP-адрес сервера и домен в файл README.md

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Деплой приложения на сервер с использованием pm2
 
 Стартеркит проекта по автоматизации деплоя фронтенда и бэкенда при помощи pm2 (pm2 deploy)
+
+IP-адрес: 62.84.117.109
+
+Frontend: https://titov.nomorepartiesco.ru
+
+Backend: https://api.titov.nomorepartiesco.ru
--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,2 +1,5 @@
 export const { JWT_SECRET = 'JWT_SECRET' } = process.env;
 export const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+// при проблеме с mongo указать вместо localhost ip адрес
+/* при проблеме с версиями noda использовать команду
+  NODE_OPTIONS=--openssl-legacy-provider && react-scripts start" */
